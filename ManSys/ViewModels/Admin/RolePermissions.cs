c9cecb6d1825c7a76614986f045423de0a5986de
Alexey <Marveleks@gmail.com>
2023-09-16 01:44:53 +0300

    Added view for permission editing

---
+++ ManSys/ViewModels/Admin/RolePermissions.cs
@@ -0,0 +1,11 @@
+﻿using ManSys.Models;
+
+namespace ManSys.ViewModels.Admin
+{
+    public class RolePermissions
+    {
+        public Role role { get; set; }
+        public List<int> permissions { get; set; }
+        
+    }
+}
